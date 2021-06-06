d57b7b95c0b59b4c2c441e5484ac9e899164f9e6
whkessler <82409360+whkessler@users.noreply.github.com>
2021-06-06 12:06:58 -0500

    first commit

---
+++ MyOwnJavaProgram.java
@@ -0,0 +1,7 @@
+public class MyOwnJavaProgram{
+    public static void main(String[] args) {
+        System.out.println("My name is William Kessler");
+        System.out.println("I am 48 years old");
+        System.out.println("My hometown is Baton Rouge,LA");
+    }
+}
