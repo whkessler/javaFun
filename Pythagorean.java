8c7276fade49cc1b366a8a6bce3dd2ff7e81e111
whkessler <82409360+whkessler@users.noreply.github.com>
2021-06-07 14:39:48 -0500

    y

---
+++ Pythagorean.java
@@ -0,0 +1,6 @@
+import java.lang.Math;
+public class Pythagorean{
+    public double calculateHypotenuse(int legA, int legB){
+        return Math.sqrt(Math.pow(legA,2)+Math.pow(legB,2));
+    }
+}
---
+++ PythagoreanTest.java
@@ -0,0 +1,8 @@
+public class PythagoreanTest{
+    public static void main(String[] args){
+        Pythagorean result = new Pythagorean();
+        System.out.println(result.calculateHypotenuse(6,8));
+        System.out.println(result.calculateHypotenuse(2,3));
+        System.out.println(result.calculateHypotenuse(12,16));
+    }
+}
